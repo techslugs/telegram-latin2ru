48dddcd39d7572efd37ad30e0e2d9f6b965b5e36
Dmitriy Dudkin <dudkin.dmitriy@gmail.com>
2016-05-26 21:33:17 +0300

    Fixed express configuration for Openshift

--- web.js
+++ web.js
@@ -7,7 +7,11 @@ app.get('/', function (req, res) {
   res.json({ version: packageInfo.version });
 });
 
-var server = app.listen(process.env.PORT, function () {
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+    port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+
+var server = app.listen(port, ipaddress, function () {
   var host = server.address().address;
   var port = server.address().port;
 
