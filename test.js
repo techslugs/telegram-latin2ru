3d7cb125e4e8eba57cfed52fc8a10af653e7a02d
Dmitriy Dudkin <dudkin.dmitriy@gmail.com>
2016-09-03 20:22:21 +0300

    Added support for raw text in square brackets (#16)
    - Added skipping text in the square brackets
     - Changed test framework to tape
     - Rewrote existing tests using tape framework
     - Added tests for mentions
     - Added tests for square brackets
     - Fixed deployment script
    
     Fixes #14

--- test.js
+++ test.js
@@ -1,15 +1,45 @@
 var latin2cyr = require('./latin2cyr');
 var translit = require('./translit.json');
-var assert = require('chai').assert;
+var test = require('tape');
 
-function _test(ru, en){
-  describe('As a user, I want to transform', function(){
-    it(en+' in to '+ru, function(){
-      assert.equal(ru, latin2cyr(en));
-    });
-  });
-}
+test('simple 1-letter translations', function (t) {
+  for(var i = 0; i < translit.length; i++){
+    var en = translit[i][0],
+        ru = translit[i][1];
+    t.equal(latin2cyr(en), ru);
+  }
+  t.end();
+});
 
-for(var i = 0; i < translit.length; i++){
-  _test(translit[i][1], translit[i][0]);
-}
+test('mentions skipping', function (t) {
+  t.equal(latin2cyr('@mention v nachale'), '@mention в начале');
+  t.equal(latin2cyr('v @mention seredine'), 'в @mention середине');
+  t.equal(latin2cyr('v konce @mention'), 'в конце @mention');
+  t.end();
+});
+
+test('square brackets skipping', function (t) {
+  t.equal(latin2cyr('[square brackets] v nachale'), '[square brackets] в начале');
+  t.equal(latin2cyr('v [square brackets] seredine'), 'в [square brackets] середине');
+  t.equal(latin2cyr('v konce [square brackets]'), 'в конце [square brackets]');
+  t.end();
+});
+
+test('single square bracket', function (t) {
+  t.equal(latin2cyr('[single v nachale'), '[сингле в начале');
+  t.equal(latin2cyr('v [single seredine'), 'в [сингле середине');
+  t.equal(latin2cyr('v konce [single'), 'в конце [сингле');
+  t.end();
+});
+
+test('multiple square brackets', function (t) {
+  t.equal(
+    latin2cyr('test [multiple] [square] [brackets] test'),
+    'тест [multiple] [square] [brackets] тест'
+  );
+  t.equal(
+    latin2cyr('test [nested [square] brackets] test'),
+    'тест [nested [square] brackets] тест'
+  );
+  t.end();
+});
